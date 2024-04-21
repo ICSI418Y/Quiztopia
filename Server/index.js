797e828a670d30ddaab34ee4db62b70be93a25a8
CreatorOfCryptids <dannypeck5000@gmail.com>
2024-04-21 04:13:29 -0400

    Added the rest of the user calls. Still untested.

--- Server/index.js
+++ Server/index.js
@@ -41,7 +41,7 @@ app.post('/createUser', async (req, res) => {
         const lastName = req.body.lastName;
         const username = req.body.username;
         const password = req.body.password;
-        console.log("/createUser " + firstName + " " + lastName + " " + username + " " + password);
+        console.log("/createUser FN:" + firstName + " LN:" + lastName + " ID:" + username + " PW:" + password);
 
         // Check that there isn't another user with the same username
         const testUsername = await User.findOne({username : username});
@@ -63,57 +63,188 @@ app.post('/createUser', async (req, res) => {
             const user = new User({firstName, lastName, username, password, folder : rootFolder._id, classes});
             await user.save()
             res.send(user);
-            console.log("New User: FN:" + firstName + " LN:" + lastName + " ID:" + username + " PW:" + password);
         }
         else {
             res.status(500).send("Username already exists")
             console.log("ERROR: Username: \"" + username + "\" already exists.");
         }
-        
     }
     catch(error){
         res.status(500).send(error);
         console.log(error);
     }
+});
+
+// - `/getUsers` - `(req:{ids : [User._id]}, res{users : [User]})` - UNTESTED
+app.get('/getUsers', async (req, res) => {
+    try{
+        const ids = req.body.ids;
+        console.log("/getUsers ids: " + ids);
+
+        let userList = [];
+
+        for(const id of ids){
+            const user = await User.findById(id);
+            userList.push({
+                firstName : user.firstName,
+                lastName : user.lastName,
+                username : user.username,
+                _id : user._id
+            });
+        }
+        res.send(userList);
+    }
+    catch (error){
+        res.status().send(error);
+        console.log(error);
+    }
 })
 
-// - `/getUsers` - `(req:{ids : [User._id]}, res{users : [User]})` - UNIMPLEMENTED
+// - `/getUser` - `(req:{_id}, res{User})` - UNTESTED
+app.get('/getUser', async (req, res) => {
+    try{
+        const id = req.body._id;
+        console.log("/getUser id: " + id);
 
-// - `/getUser` - `(req:{_id}, res{User})` - UNIMPLEMENTED
+        const user = await User.findById(id);
 
-// - `/getUserByUserName` - `(req:{userName}, res{User})` - UNIMPLEMENTED
+        let retUser = {
+            firstName : user.firstName,
+            lastName : user.lastName,
+            username : user.username,
+            _id : user._id
+        }
 
-// - `/deleteUser` - `(req:{_id}, res{})` - UNIMPLEMENTED 
+        res.send(retUser);
+    }
+    catch (error){
+        res.status().send(error);
+        console.log(error);
+    }
+})
+
+// - `/getUserByUsername` - `(req:{userName}, res{User})` - UNTESTED
+app.get('/getUserByUsername', async (req, res) => {
+    try{
+        const username = req.body.username;
+        console.log("/getUserByName UN: " + username);
+
+        const user = await User.findOne({username : username});
+
+        let retUser = {
+            firstName : user.firstName,
+            lastName : user.lastName,
+            username : user.username,
+            _id : user._id
+        }
+        res.send(retUser);
+    }
+    catch (error){
+        res.status().send(error);
+        console.log(error);
+    }
+})
+
+// `/loginUser` - `(req:{username, password}, res{User})` - UNTESTED
+app.get('/loginUser', async (req, res) => {
+    try{
+        const username = req.body.username;
+        const password = req.body.password
+        console.log("/getUserByName UN: " + username + " PW: " + password);
+
+        const user = await User.findOne({username : username, password : password});
+
+        res.send(user);
+    }
+    catch (error){
+        res.status().send(error);
+        console.log(error);
+    }
+})
+
+// - `/deleteUser` - `(req:{_id}, res{})` - UNTESTED 
+app.delete('/deleteUser', async (req, res) =>{
+    try{
+        const _id = req.body._id;
+        console.log("/deleteUser _id: " + _id);
+
+        await User.deleteOne({_id : _id});
+    }
+    catch(error){
+        res.status().send(error);
+        console.log(error);
+    }
+});
 
 
 // - `/createFolder` - `(req : {title : String, parent : Folder._id}, res{})` - UNIMPLEMENTED
 
 // - `/getFolderByID` - `(req : {_id}, res : {title : String, parent : SmallFolder, children : [SmallFolder], sets : [SmallSet]})` - UNIMPLEMENTED
-
-// - `/getCopy` - `(req : {_id}, res {Folder._id})` - UNIMPLEMENTED
+app.get('', async (req, res) => {
+    try{
+        
+    }
+    catch (error){
+        res.status().send(error);
+        console.log(error);
+    }
+})
 
 // - `/editChildren` - `(req : {_id, children [Folder._id]}, res {})` - UNIMPLEMENTED
 
 // - `/editSets` - `(req : {_id, sets [Set._id]}, res {})` - UNIMPLEMENTED
 
 // - `/deleteFolder` - `(req:{_id}, res{})` - UNIMPLEMENTED
+app.delete('/deleteFolder', async (req, res) =>{
+    try{
 
+    }
+    catch(error){
+        res.status().send(error);
+        console.log(error);
+    }
+});
 
 // - `/createClass` - `(req : {owner : User._id, teachers : [User._id], students : [User._id]}, res {})` - UNIMPLEMENTED
 
 // - `/getClass` - `(req : {}, res {})` - UNIMPLEMENTED
+app.get('', async (req, res) => {
+    try{
+        
+    }
+    catch (error){
+        res.status().send(error);
+        console.log(error);
+    }
+})
 
 // - `/editStudents` - `(req : {students : [User._id]}, res {})` - UNIMPLEMENTED
 
 // - `/editTeachers` - `(req : {teachers : [User._id]}, res {})` - UNIMPLEMENTED
 
+// - `/deleteClass` - `(req : )`
+app.delete('/deleteClass', async (req, res) =>{
+    try{
 
-// - `/getRoster` - `(req:{users : [User]}, res{})` - UNIMPLEMENTED
-
-// - `/editRoster` - `(req:{_id, users : Ow[User]}, res{_id})` - UNIMPLEMENTED
-
+    }
+    catch(error){
+        res.status().send(error);
+        console.log(error);
+    }
+});
 
 // - `/createSet` - `(req:{title : String, description : String, flashcards : [Flashcard]}, res{})` - UNIMPLEMENTED
+
+// - `/getSet` - `(req:(Set._id), res(Set, [Flashcard]))` - UNIMPLEMENTED
+app.get('', async (req, res) => {
+    try{
+        
+    }
+    catch (error){
+        res.status().send(error);
+        console.log(error);
+    }
+})
 
 // - `/editTitle` - `(req:{title : String}, res{})` - UNIMPLEMENTED
 
@@ -121,14 +252,40 @@ app.post('/createUser', async (req, res) => {
 
 // - `/editCards` - `(req:{oldCard : Flashcard._id, newCard : Flashcard}, res{})` - UNIMPLEMENTED
 
-// - `/deleteCard` - `(req:{set: Set._id, card : Flashcard._id}, res{Flashcard})` - UNIMPLEMENTED
-
 // - `/deleteSet` - `(req:{_id}, res{})` - UNIMPLEMENTED
+app.delete('/deleteSet', async (req, res) =>{
+    try{
+
+    }
+    catch(error){
+        res.status().send(error);
+        console.log(error);
+    }
+});
 
 
 // - `/getCard` - `(req:{card : Flashcard._id}, res{Flashcard})` - UNIMPLEMENTED
+app.get('', async (req, res) => {
+    try{
+        
+    }
+    catch (error){
+        res.status().send(error);
+        console.log(error);
+    }
+})
 
 // - `/editCard` - `(req:{term : String, definition : Stringt}, res{})` - UNIMPLEMENTED
 
 // - `/updateProfficiency` - `(req:{profficiency : Number}, res{})` - UNIMPLEMENTED
 
+// - `/deleteCard` - `(req:{set: Set._id, card : Flashcard._id}, res{Flashcard})` - UNIMPLEMENTED
+app.delete('/deleteCard', async (req, res) =>{
+    try{
+
+    }
+    catch(error){
+        res.status().send(error);
+        console.log(error);
+    }
+});
