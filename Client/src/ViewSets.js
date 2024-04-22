2aea24c25b60e6e252cd30496bb9aceba4ea44cd
dporter105 <142933423+dporter105@users.noreply.github.com>
2024-04-22 11:23:16 -0400

    Create ViewSets.js

---
+++ Client/src/ViewSets.js
@@ -0,0 +1,34 @@
+import { React, useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ViewCardSet= () => {
+    const [CardSets, setCardSets] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:9000/getSets')
+        .then(function (response) {
+            setCardSets(response.data);
+            console.log('sets', response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }, []);
+
+    return (
+        <form>
+            <div>
+                <h1>Flash Card Sets</h1>
+                <ul>
+                    {CardSets.map((sets) => (
+                        <li key={sets._id}>
+                            <Link to={'/ViewCardSet/${set._id'}>{set.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </form>
+    );
+}
+
+export default ViewCardSet;
