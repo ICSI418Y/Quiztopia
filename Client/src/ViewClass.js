e320c9710d134e9a8871d83ee091ab30674214bf
mendelsshop <mendelsshop@gmail.com>
2024-04-22 18:18:50 -0400

    [Client] added some missing imports

--- Client/src/ViewClass.js
+++ Client/src/ViewClass.js
@@ -1,44 +1,45 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import './index.css';
 import './App.css';
 
-    const ViewClass = () => {
-        const [thisClass, setClass] = useState([])
-        const [studentRoster, setStudentRoster] = useState([])
-        const [teacherRoster, setTeacherRoster] = useState([])
-        const studentNames = []
-        const teacherNames = []
-        useEffect(() => {
-            axios.get('http://localhost:9000/getClass').then((res)=>setClass(res.data))
-            axios.get('http://localhost:9000/getRoster', thisClass.students).then((res)=>setStudentRoster(res.data))
-            axios.get('http://localhost:9000/getRoster', thisClass.teachers).then((res)=>setTeacherRoster(res.data))
-        })
-        const studentUsers = studentRoster.users
-        const teacherUsers = teacherRoster.users
-        for(let i=0; i<studentRoster.users.length; i++){
-            const name = studentRoster.users[i].firstName + studentRoster.users[i].lastName
-            studentNames[i] = name
-        }
-        for(let i=0; i<teacherRoster.users.length; i++){
-            const name = teacherRoster.users[i].firstName + teacherRoster.users[i].lastName
-            teacherNames[i] = name
-        }
-        return(
-            <form className="center background">
-                <div>
-                    Names of teachers in class: {this.props.teacherNames.map(txt => <p>{txt}</p>)}
-                </div>
-                <div>
-                    Names of students in class: {this.props.studentNamesNames.map(txt => <p>{txt}</p>)}
-                </div>
-                <p>
-                    Would you like to edit your class? <Link to="/EditClass">Folders</Link>
-                </p>
-                <p>
-                    Would you like to view your folders? <Link to="/ViewFolder">Folders</Link>
-                </p>
-            </form>
-        )
+const ViewClass = () => {
+    const [thisClass, setClass] = useState([])
+    const [studentRoster, setStudentRoster] = useState([])
+    const [teacherRoster, setTeacherRoster] = useState([])
+    const studentNames = []
+    const teacherNames = []
+    useEffect(() => {
+        axios.get('http://localhost:9000/getClass').then((res) => setClass(res.data))
+        axios.get('http://localhost:9000/getRoster', thisClass.students).then((res) => setStudentRoster(res.data))
+        axios.get('http://localhost:9000/getRoster', thisClass.teachers).then((res) => setTeacherRoster(res.data))
+    })
+    const studentUsers = studentRoster.users
+    const teacherUsers = teacherRoster.users
+    for (let i = 0; i < studentRoster.users.length; i++) {
+        const name = studentRoster.users[i].firstName + studentRoster.users[i].lastName
+        studentNames[i] = name
     }
-    export default ViewClass;
+    for (let i = 0; i < teacherRoster.users.length; i++) {
+        const name = teacherRoster.users[i].firstName + teacherRoster.users[i].lastName
+        teacherNames[i] = name
+    }
+    return (
+        <form className="center background">
+            <div>
+                Names of teachers in class: {this.props.teacherNames.map(txt => <p>{txt}</p>)}
+            </div>
+            <div>
+                Names of students in class: {this.props.studentNamesNames.map(txt => <p>{txt}</p>)}
+            </div>
+            <p>
+                Would you like to edit your class? <Link to="/EditClass">Folders</Link>
+            </p>
+            <p>
+                Would you like to view your folders? <Link to="/ViewFolder">Folders</Link>
+            </p>
+        </form>
+    )
+}
+export default ViewClass;
--- Client/src/ViewFolder.js
+++ Client/src/ViewFolder.js
@@ -1,79 +1,80 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import './index.css';
 import './App.css';
 
-    const ViewFolder = () => {
-        const[folder,setFolder] = useState([])
-        const[currentFolder_id,setcurrentFolder_id] = useState([])
-        const[folderName,setfolderName] = useState('')
-        const[folderDelete_id,setfolderDelete_id] = useState([])
-        const[selectedSets,setSelectedSets] = useState([])
-        const currentSets = []
-        const handleCreateFolder = (event, folderName, folder_id) => {
-            event.preventDefault()
-            axios.post('http://localhost:9000/createFolder', {folderName, folder_id})
+const ViewFolder = () => {
+    const [folder, setFolder] = useState([])
+    const [currentFolder_id, setcurrentFolder_id] = useState([])
+    const [folderName, setfolderName] = useState('')
+    const [folderDelete_id, setfolderDelete_id] = useState([])
+    const [selectedSets, setSelectedSets] = useState([])
+    const currentSets = []
+    const handleCreateFolder = (event, folderName, folder_id) => {
+        event.preventDefault()
+        axios.post('http://localhost:9000/createFolder', { folderName, folder_id })
             .catch((err) => alert('Error in Creating Folder'))
-        }
-        const handleDeleteFolder = (event, folder_id) => {
-            event.preventDefault()
-            axios.post('http://localhost:9000/deleteFolder', {folder_id})
+    }
+    const handleDeleteFolder = (event, folder_id) => {
+        event.preventDefault()
+        axios.post('http://localhost:9000/deleteFolder', { folder_id })
             .catch((err) => alert('Error in Deleting Folder'))
-        }
-        const handleGetFolder = ()=>{
-            axios.get('http://localhost:9000/getFolderById', currentFolder_id).then((res)=>setFolder(res.data))
-            currentSets = folder.sets.map((sets) => {
-                return {label: sets.title, value: sets._id}
-            })
-        }
-    return(
+    }
+    const handleGetFolder = () => {
+        axios.get('http://localhost:9000/getFolderById', currentFolder_id).then((res) => setFolder(res.data))
+        currentSets = folder.sets.map((sets) => {
+            return { label: sets.title, value: sets._id }
+        })
+    }
+    return (
         <div className="center background">
             <h1>{folder.title}</h1>
             <label>Input desired folder's id
-                <input type="text" value={currentFolder_id} onChange={(e) => setcurrentFolder_id(e.target.value)}/>
+                <input type="text" value={currentFolder_id} onChange={(e) => setcurrentFolder_id(e.target.value)} />
             </label>
             <button className='loginButtonSpacing' onClick={handleGetFolder}>Get Folder</button>
             <label>Switch view to child folder
                 <select onChange={(e) => setFolder(e.target.value)} value={folder}>
                     <option value="">Select Folder</option>
-                        {folder.children.map((folder, index) => {
-                        return <option key={index} value={folder._id}>   
-                        {folder.title}
-                    </option>
+                    {folder.children.map((folder, index) => {
+                        return <option key={index} value={folder._id}>
+                            {folder.title}
+                        </option>
                     })
                     }
                 </select>
             </label>
             <label>Create new child folder
                 <label>Name of folder
-                    <input type="text" value={folderName} onChange={(e) => setfolderName(e.target.value)}/>
+                    <input type="text" value={folderName} onChange={(e) => setfolderName(e.target.value)} />
                 </label>
             </label>
             <button className='loginButtonSpacing' type="button" onClick={(event) => handleCreateFolder(event, folderName, folder._id)}>
-                    Create Folder
+                Create Folder
             </button>
             <select onChange={(e) => setfolderDelete_id(e.target.value)} value={folderDelete_id}>
-                    <option value="">Select Folder to Delete</option>
-                        {folder.children.map((folder, index) => {
-                        return <option key={index} value={folder._id}>   
+                <option value="">Select Folder to Delete</option>
+                {folder.children.map((folder, index) => {
+                    return <option key={index} value={folder._id}>
                         {folder.title}
                     </option>
-                    })
-                    }
-                </select>
+                })
+                }
+            </select>
             <button className='loginButtonSpacing' type="button" onClick={(event) => handleDeleteFolder(event, folderDelete_id)}>
                 Delete Folder
             </button>
             <select onChange={(e) => setSelectedSets(e.target.value)} value={selectedSets}>
                 <option value="">Select Set to open</option>
-                    {currentSets.map((sets, index) => {
-                    return <option key={index} value={sets._id}>   
-                    {sets.title}
-                </option>
+                {currentSets.map((sets, index) => {
+                    return <option key={index} value={sets._id}>
+                        {sets.title}
+                    </option>
                 })
                 }
             </select>
-            <button className='loginButtonSpacing' onClick={(e) => <Link to ="/ViewSet"></Link>}>ViewSet</button>  
+            <button className='loginButtonSpacing' onClick={(e) => <Link to="/ViewSet"></Link>}>ViewSet</button>
         </div>
     )
 }
