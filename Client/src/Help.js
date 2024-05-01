a808e6b123648e8411dec5293d4ecbb2227a2463
mniemczyk123 <142933704+mniemczyk123@users.noreply.github.com>
2024-05-01 17:27:38 -0400

    Add files via upload

---
+++ Client/src/Help.js
@@ -0,0 +1,38 @@
+const Help = () =>{
+    return(
+        <div>
+        <form>
+            <label>
+            Help
+            </label>
+            <br/>
+            <br/>
+            <label>
+            Commonly asked questions
+            </label>
+            <br/>
+            <label>
+            Q: If I delete a folder, does it delete all of the folders beneath it
+            <br/>
+            A: Yes, if you want to save info from any child folders copy them and move them somewhere else first
+            </label>
+            <br/>
+            <br/>
+            <label>
+            Q: I accidently deleted a folder that contained everything, can I get it back
+            <br/>
+            A: No, sorry
+            </label>
+            <br/>
+            <br/>
+            <label>
+            Contact Support at:
+            Phone number:(123)-456-7890
+            Email: quiztopia@quiz.com
+            </label>
+            <br/>
+            </form>
+            </div>
+    )
+}
+export default Help;
